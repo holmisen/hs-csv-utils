2bd13f31d7b88181b2554291c88b55cdf902ba7b
Johan Holmquist <holmisen@gmail.com>
2013-10-05 13:01:11 +0200

    Resizable and fixed sort bug

--- src/csv-table.hs
+++ src/csv-table.hs
@@ -15,7 +15,7 @@ main = do
   ls <- lines `fmap` getContents
   let sep = maybe defaultSep id $ listToMaybe args
 
-  -- Setup data
+  -- Parse data
   let ldata = map (mkRow sep) ls
   let cols  = maximum $ map arraySize ldata
 
@@ -24,8 +24,11 @@ main = do
   win <- windowNew
   onDestroy win mainQuit
 
+  sw <- scrolledWindowNew Nothing Nothing
+  set win [containerChild := sw]
+
   (store,view) <- createTable cols ldata
-  set win [containerChild := view]
+  set sw [containerChild := view]
 
   widgetShowAll win
   mainGUI
@@ -38,6 +41,9 @@ mkRow sep s = listArray (1,n) ss where
 
 arraySize a = let (l,h) = bounds a in 1 + h - l
 
+ixDefault def a i = if l <= i && i <= h then a!i else def where
+    (l,h) = bounds a
+
 
 createTable cols tableData = do
   store <- listStoreNew tableData
@@ -46,7 +52,7 @@ createTable cols tableData = do
 
   -- Add each column
   forM_ [1..cols] $ \i -> do
-    col <- createSortedColumn store sorted i (show i) $ \tr -> [cellText := (tr!i)]
+    col <- createSortedColumn store sorted i (show i) $ \tr -> [cellText := ixDefault [] tr i]
     treeViewAppendColumn view col
 
   treeViewSetEnableSearch view True
@@ -56,22 +62,21 @@ createTable cols tableData = do
     return $ any (isPrefixOf str) $ elems row
 
   return (store, view)
-  where
-    eqf s it = return False -- TODO
 
 
 createSortedColumn store sortedStore colId title f = do
   col <- treeViewColumnNew
   set col [treeViewColumnTitle := title]
-  treeViewColumnSetSortColumnId col colId
+  treeViewColumnSetResizable col True
 
   cell <- cellRendererTextNew
   cellLayoutPackStart col cell False
   cellLayoutSetAttributes col cell store f
 
+  treeViewColumnSetSortColumnId col colId
   treeSortableSetSortFunc sortedStore colId $ \l r -> do
     x <- treeModelGetRow store l
     y <- treeModelGetRow store r
-    return (compare (x!colId) (y!colId))
+    return (compare (ixDefault [] x colId) (ixDefault [] y colId))
 
   return col
